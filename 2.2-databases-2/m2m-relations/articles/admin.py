b548d26c5af8e5b5615d8fac0651b8df057ebec7
Alexander Bugaev <iustusvir@gmail.com>
2023-07-30 20:00:49 +0300

    articles

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    
+    def clean(self):
+        count_true = [form.cleaned_data.get('is_main') for form in self.forms].count(True)
+        if count_true < 1:
+            raise ValidationError('Укажите основной раздел')
+        elif count_true > 1:
+            raise ValidationError('Основным может быть только один раздел !!!')
+        return super().clean()
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 1
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ScopeInline]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
+            
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -14,3 +14,28 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+
+    articles = models.ManyToManyField(Article, through='Scope')
+    name = models.CharField(max_length=200, verbose_name='Имя тега')
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
+    
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Scope(models.Model):
+
+    article = models.ForeignKey(Article, related_name='scopes', on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, related_name='scopes', on_delete=models.CASCADE)
+    is_main = models.BooleanField(verbose_name='ОСНОВНОЙ')
+
+    class Meta:
+        verbose_name = 'тэг статьи'
+        verbose_name_plural = 'Тэги статьи'
+        ordering = ['-is_main', 'tag']
