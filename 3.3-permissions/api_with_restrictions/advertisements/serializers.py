96931dcaa5d582e477eb7ade72908e101050ec4f
Alexander Bugaev <iustusvir@gmail.com>
2023-08-18 18:46:21 +0300

    advertisements restrictions

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
+from rest_framework.exceptions import ValidationError
 from advertisements.models import Advertisement
 
 
@@ -27,19 +27,18 @@ class AdvertisementSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         """Метод для создания"""
-
-        # Простановка значения поля создатель по-умолчанию.
-        # Текущий пользователь является создателем объявления
-        # изменить или переопределить его через API нельзя.
-        # обратите внимание на `context` – он выставляется автоматически
-        # через методы ViewSet.
-        # само поле при этом объявляется как `read_only=True`
         validated_data["creator"] = self.context["request"].user
         return super().create(validated_data)
 
     def validate(self, data):
         """Метод для валидации. Вызывается при создании и обновлении."""
-
-        # TODO: добавьте требуемую валидацию
-
+        open_qty = Advertisement.objects.filter(creator=self.context['request'].user).\
+            filter(status='OPEN').count()>=10
+        if self.instance.status == data['status']:
+            raise ValidationError(f'Текущее обьявление уже имеет статус {data["status"]}')
+        elif 'status' in data and data['status'] =='OPEN' and open_qty:
+            raise ValidationError('Превышен лимит открытых обьявлений!')
+        elif self.context['view'].action == 'create' and open_qty:
+            raise ValidationError('Новое обьявление превысит лимит открытых обьявлений.\
+                                   Закройте либо удалите одно из существующих')
         return data
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,15 +1,27 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from django.contrib.auth.models import User
+from .models import Advertisement
+from .serializers import AdvertisementSerializer, UserSerializer
+from .filters import AdvertisementFilter
+from .permissions import IsSuperuserOrOwnerOrReadOnly
 
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class AdvertisementViewSet(ModelViewSet):
-    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
 
-    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
-    #   сериализаторов и фильтров
+    """ViewSet для объявлений."""
 
     def get_permissions(self):
         """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+            return [IsAuthenticated(), IsSuperuserOrOwnerOrReadOnly()]
         return []
+ 
